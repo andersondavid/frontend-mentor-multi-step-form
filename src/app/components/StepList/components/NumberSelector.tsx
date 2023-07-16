d24906d0ac940cbb2880a9e8e1847364da1765b0
Anderson David SF <andersondavidprofissional@gmail.com>
2023-07-16 00:52:39 -0300

    fix(NumberSelector): Pass white class to condition

--- src/app/components/StepList/components/NumberSelector.tsx
+++ src/app/components/StepList/components/NumberSelector.tsx
@@ -9,10 +9,12 @@ export default function NumberSelector({ number, active }: PropsTypes) {
   return (
     <span
       className={`${
-        active ? "bg-light-blue" : ''
+        active ? 'bg-light-blue' : ''
       } flex h-8 w-8 items-center justify-center rounded-full border-[1px] border-solid border-light-blue`}
     >
-      <p className={`${active ? "text-marine-blue" : ""} text-white`}>{number}</p>
+      <p className={`${active ? 'text-marine-blue' : 'text-white '} `}>
+        {number}
+      </p>
     </span>
   )
 }
