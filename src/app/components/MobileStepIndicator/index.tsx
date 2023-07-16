3a7784d8d87f4943bf18b9c875fda08f0da195ea
Anderson David SF <andersondavidprofissional@gmail.com>
2023-07-16 11:51:50 -0300

    refactor: Change 'StepList' to 'MobileStepIndicator' name

---
+++ src/app/components/MobileStepIndicator/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NumberSelector from './components/NumberSelector'
+
+export default function MobileStepIndicator() {
+  return (
+    <div className="w-full py-8">
+      <div className="flex h-full w-full flex-row justify-center gap-4">
+        <NumberSelector number={'1'} active={true} />
+        <NumberSelector number={'2'} />
+        <NumberSelector number={'3'} />
+        <NumberSelector number={'4'} />
+      </div>
+    </div>
+  )
+}
