4c84a9b2b7d7d9a81c31d3311a0a3d3cc6983789
Anderson David SF <andersondavidprofissional@gmail.com>
2023-07-16 11:55:37 -0300

    feat(Home):  Add Screens to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import StepList from './components/MobileStepIndicator'
+import Screens from './components/Screens'
+
 export default function Home() {
   return (
-    <main className="">
-     
+    <main className="flex h-screen w-screen relative">
+      <div className="grow">
+        <div className='bg-sidebar-mobile absolute h-1/4 w-full -z-10'></div>
+        <StepList />
+        <Screens />
+      </div>
     </main>
   )
 }
