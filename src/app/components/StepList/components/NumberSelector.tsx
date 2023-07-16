9b3cd1b2e419cd749e520c6c31ec517f84a56b23
Anderson David SF <andersondavidprofissional@gmail.com>
2023-07-16 00:26:05 -0300

    feat(NumberSelector): Create Number Selector

---
+++ src/app/components/StepList/components/NumberSelector.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type PropsTypes = {
+  number: string
+  active?: boolean
+}
+
+export default function NumberSelector({ number, active }: PropsTypes) {
+  return (
+    <span
+      className={`${
+        active ? "bg-light-blue" : ''
+      } flex h-8 w-8 items-center justify-center rounded-full border-[1px] border-solid border-light-blue`}
+    >
+      <p className={`${active ? "text-marine-blue" : ""} text-white`}>{number}</p>
+    </span>
+  )
+}
