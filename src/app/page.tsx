629c17ee43313cd4d1367cea2dceaf0db8f8ee65
Anderson David SF <andersondavidprofissional@gmail.com>
2023-07-16 23:54:43 -0300

    fix: Set background-size:  cover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex h-screen w-screen relative">
       <div className="grow">
-        <div className='bg-sidebar-mobile absolute h-1/4 w-full -z-10'></div>
+        <div className='bg-sidebar-mobile bg-cover absolute h-1/4 w-full -z-10'></div>
         <StepList />
         <Screens />
       </div>
