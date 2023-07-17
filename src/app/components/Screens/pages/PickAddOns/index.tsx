b06b6002772e86021aea425d69ae1f2e616b7703
Anderson David SF <andersondavidprofissional@gmail.com>
2023-07-16 23:49:11 -0300

    feat(PickAddOns): Create Pick Add-Ons

---
+++ src/app/components/Screens/pages/PickAddOns/index.tsx
@@ -0,0 +1,111 @@
+'use client'
+
+import React, { useState } from 'react'
+import AddOnsSelectOption from './components/AddOnsSelectOption'
+
+type PlanItemAddOns = {
+  name: string
+  checked: boolean
+  title: string
+  desc: string
+  price: string
+}
+
+enum Duration {
+  Monthy = 'MONTHY',
+  Yearly = 'YEARLY',
+}
+
+const monthlyPlansAddOns: PlanItemAddOns[] = [
+  {
+    name: 'onlineServices',
+    title: 'Online service',
+    price: '+$1/mo',
+    checked: false,
+    desc: 'Access to multiplayer games',
+  },
+  {
+    name: 'largerStorage',
+    title: 'Larger storate',
+    price: '+$2/mo',
+    checked: false,
+    desc: 'Extra 1TB of cloud save',
+  },
+  {
+    name: 'customProfile',
+    title: 'Customizable profile',
+    price: '+$2/mo',
+    checked: false,
+    desc: 'Custom theme on your profile',
+  },
+]
+const yearlyPlansAddOns: PlanItemAddOns[] = [
+  {
+    name: 'onlineServices',
+    title: 'Online service',
+    price: '+$10/yr',
+    checked: false,
+    desc: 'Access to multiplayer games',
+  },
+  {
+    name: 'largerStorage',
+    title: 'Larger storate',
+    price: '+$20/ye',
+    checked: false,
+    desc: 'Extra 1TB of cloud save',
+  },
+  {
+    name: 'customProfile',
+    title: 'Customizable profile',
+    price: '+$20/yr',
+    checked: false,
+    desc: 'Custom theme on your profile',
+  },
+]
+
+export default function PickAddOns() {
+  const [typeDurationAddOns, setTypeDurationAddOns] =
+    useState<PlanItemAddOns[]>(monthlyPlansAddOns)
+
+  const handleCheckboxChange = (name: string) => {
+    setTypeDurationAddOns(
+      typeDurationAddOns.map((item) => {
+        if (item.name === name) {
+          return {
+            ...item,
+            checked: !item.checked,
+          }
+        }
+        return item
+      })
+    )
+
+    console.log(typeDurationAddOns)
+  }
+
+  console.log('RENDER')
+
+  return (
+    <article>
+      <h2 className="page-title">Pick add-ons</h2>
+      <p className="page-desc text-cool-gray">
+        Add-ons help enhance your gaming experience.
+      </p>
+      <div>
+        {typeDurationAddOns.map((item) => {
+          return (
+            <span onClick={() => handleCheckboxChange(item.name)}>
+              <AddOnsSelectOption
+                checked={item.checked}
+                desc={item.desc}
+                title={item.title}
+                price={item.price}
+                key={item.title}
+              />
+            </span>
+          )
+        })}
+      </div>
+    </article>
+  )
+}
