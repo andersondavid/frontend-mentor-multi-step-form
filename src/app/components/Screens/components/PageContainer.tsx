58fcba53270a4ae0e79e420560af6c6a57aaf5d1
Anderson David SF <andersondavidprofissional@gmail.com>
2023-07-22 13:00:45 -0300

    fix: Fix PageContainer padding

--- src/app/components/Screens/components/PageContainer.tsx
+++ src/app/components/Screens/components/PageContainer.tsx
@@ -6,7 +6,7 @@ type PropsTypes = {
 
 export default function PageContainer({ children }: PropsTypes) {
   return (
-    <div className="m-auto flex max-w-4xl items-stretch rounded-xl bg-white md:p-4">
+    <div className="m-auto px-6 py-9 md:flex max-w-4xl items-stretch rounded-xl bg-white md:p-4">
       {children}
     </div>
   )
