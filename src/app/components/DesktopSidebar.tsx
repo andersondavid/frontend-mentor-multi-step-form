18d60c4941fd1000e5c914b3a6a0bb4b3acd6a10
Anderson David SF <andersondavidprofissional@gmail.com>
2023-08-13 21:44:27 -0300

    feat: Number selector fixed

--- src/app/components/DesktopSidebar.tsx
+++ src/app/components/DesktopSidebar.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import NumberSelector from './NumberSelector'
+import { Context, Pages } from '@/store/context'
 
 export default function DesktopSidebar() {
+
+  const {state, dispatch } = useContext(Context)
+
   return (
     <div className="mr-4 hidden h-[568px] w-[275px] md:block">
       <div className="h-full w-full rounded-xl bg-purplish-blue bg-sidebar-desktop bg-cover px-2 py-3">
         <div className="m-6 flex items-center">
-          <NumberSelector number="1" key="1" />
+          <NumberSelector number="1" active={state.currentPage == Pages.PERSONAL_INFO} />
           <div className="ml-5">
             <h3 className="font-ubuntu text-sm leading-4 text-cool-gray">
               STEP 1
@@ -17,7 +21,7 @@ export default function DesktopSidebar() {
           </div>
         </div>
         <div className="m-6 flex items-center">
-          <NumberSelector number="1" key="1" />
+          <NumberSelector number="2" active={state.currentPage == Pages.SELECT_YOUR_PLAN} />
           <div className="ml-5">
             <h3 className="font-ubuntu text-sm leading-4 text-cool-gray">
               STEP 2
@@ -28,7 +32,7 @@ export default function DesktopSidebar() {
           </div>
         </div>
         <div className="m-6 flex items-center">
-          <NumberSelector number="1" key="1" />
+          <NumberSelector number="3" active={state.currentPage == Pages.PICK_ADDONS} />
           <div className="ml-5">
             <h3 className="font-ubuntu text-sm leading-4 text-cool-gray">
               STEP 3
@@ -39,7 +43,7 @@ export default function DesktopSidebar() {
           </div>
         </div>
         <div className="m-6 flex items-center">
-          <NumberSelector number="1" key="1" />
+          <NumberSelector number="4" active={state.currentPage == Pages.FINISHING_UP || state.currentPage == Pages.THANK_YOU} />
           <div className="ml-5">
             <h3 className="font-ubuntu text-sm leading-4 text-cool-gray">
               STEP 4
--- src/app/components/MobileStepIndicator.tsx
+++ src/app/components/MobileStepIndicator.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import NumberSelector from './NumberSelector'
+import { Context, Pages } from '@/store/context'
 
 export default function MobileStepIndicator() {
+  const { state } = useContext(Context)
+
   return (
     <div className="w-full py-8 md:hidden">
       <div className="flex h-full w-full flex-row justify-center gap-4">
-        <NumberSelector number={'1'} active={true} />
-        <NumberSelector number={'2'} />
-        <NumberSelector number={'3'} />
-        <NumberSelector number={'4'} />
+        <NumberSelector number={'1'} active={state.currentPage == Pages.PERSONAL_INFO} />
+        <NumberSelector number={'2'} active={state.currentPage == Pages.SELECT_YOUR_PLAN} />
+        <NumberSelector number={'3'} active={state.currentPage == Pages.PICK_ADDONS} />
+        <NumberSelector number={'4'} active={state.currentPage == Pages.FINISHING_UP} />
       </div>
     </div>
   )
